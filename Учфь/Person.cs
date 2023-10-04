9e0f9872ddc6af5c3a02d578eaa3d2818166f40e
AkhunovAziz <akhunovaziz735@gmail.com>
2023-10-04 21:01:59 +0500

    new

---
+++ Учфь/Person.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Учфь
+{
+    public class Person
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+    }
+}
---
+++ Учфь/Program.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Учфь;
+
+string jsonObj = @"{
+    ""userId"": 1,
+    ""id"": 1,
+    ""title"": ""sunt aut facere repellat provident occaecati excepturi optio reprehenderit"",
+    ""body"": ""quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto""
+  }";
+
+
+var person = JsonConvert.DeserializeObject<Person>(jsonObj);
+Console.WriteLine($"{person.userId}|{person.id}|{person.title}|{person.body}");
